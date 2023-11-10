4b4e14b8e220a465a5cb258fea84dc75bc982909
anthonybacon916@gmail.com <anthonybacon916@gmail.com>
2023-11-10 02:38:23 +0000

    enter key functionality

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -6,10 +6,17 @@ function SearchBar(props) {
     const [inputValue, setinputValue] = useState("")
     const search = () => {props.search(inputValue)};
 
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            search();
+        }
+    }
+
+
     return (
         <div className="wrap">
             <div className="search" >
-                <input type="text" placeholder="Enter a word" className="searchTerm" onChange={(e) => {setinputValue(e.target.value)}}/>
+                <input type="text" placeholder="Enter a word" className="searchTerm" onChange={(e) => {setinputValue(e.target.value)}} onKeyPress={handleKeyPress}/>
                 <button className='searchButton' onClick={search}> Search </button>
             </div>
         </div>
